458a22f7d8e990a712a4f578f9c80a5f8ab47f49
JayusJay <atayure@gmail.com>
2025-02-05 14:24:23 +0000

    feat: Integrate RainbowKit and Wagmi for enhanced wallet management and query handling

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,68 +2,31 @@ import "./App.css"
 import { BrowserRouter } from "react-router-dom"
 import AppRoutes from "./routes"
 
-import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react"
-//import { BrowserProvider } from 'ethers'
+import "@rainbow-me/rainbowkit/styles.css"
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import { WagmiProvider } from "wagmi"
+import { arbitrum, flowMainnet } from "wagmi/chains"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
 
-const projectId = import.meta.env.VITE_PROJECT_ID
-
-// const arbitrum_sepolia = {
-//     chainId: 421614,
-//     name: 'Arbitrum Sepolia',
-//     currency: 'ETH',
-//     explorerUrl: "https://sepolia.arbiscan.io/",
-//     rpcUrl : "https://endpoints.omniatech.io/v1/arbitrum/sepolia/public	"
-// }
-
-const arbitrum = {
-    chainId: 42161,
-    name: "Arbitrum",
-    currency: "ETH",
-    explorerUrl: "https://arbiscan.io",
-    rpcUrl: "https://arb1.arbitrum.io/rpc",
-}
-
-const flowEvm = {
-    chainId: 747,
-    name: "EVM on Flow",
-    currency: "FLOW",
-    explorerUrl: "https://evm.flowscan.io/",
-    rpcUrl: "https://mainnet.evm.nodes.onflow.org",
-}
-
-const metadata = {
-    name: "My Website",
-    description: "My Website description",
-    url: "https://mywebsite.com", // origin must match your domain & subdomain
-    icons: ["https://avatars.mywebsite.com/"],
-}
-
-const ethersConfig = defaultConfig({
-    /*Required*/
-    metadata,
-
-    /*Optional*/
-    // enableEIP6963: true, // true by default
-    enableInjected: true, // true by default
-    enableCoinbase: true, // true by default
-    rpcUrl: "...", // used for the Coinbase SDK
-    defaultChainId: 1, // used for the Coinbase SDK
+const config = getDefaultConfig({
+    appName: "SafeYields",
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    chains: [arbitrum, flowMainnet],
+    ssr: false, // If your dApp uses server side rendering (SSR)
 })
-
-createWeb3Modal({
-    ethersConfig,
-    chains: [arbitrum, flowEvm],
-    projectId,
-    enableAnalytics: true, // Optional - defaults to your Cloud configuration
-})
+const queryClient = new QueryClient()
 
 function App() {
     return (
-        <>
-            <BrowserRouter>
-                <AppRoutes />
-            </BrowserRouter>
-        </>
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider>
+                    <BrowserRouter>
+                        <AppRoutes />
+                    </BrowserRouter>
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
     )
 }
 
