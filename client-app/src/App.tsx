89e5615d7a860971c146807d2497d632bdc95f10
JayusJay <atayure@gmail.com>
2025-02-05 13:25:26 +0000

    feat: Update Web3 configuration and improve project structure

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,65 +1,70 @@
-import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes";
+import "./App.css"
+import { BrowserRouter } from "react-router-dom"
+import AppRoutes from "./routes"
 
-import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/react'
-import { BrowserProvider } from 'ethers'
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react"
+//import { BrowserProvider } from 'ethers'
 
-const projectId = 'YOUR_PROJECT_ID'
+const projectId = import.meta.env.VITE_PROJECT_ID
 
+// const arbitrum_sepolia = {
+//     chainId: 421614,
+//     name: 'Arbitrum Sepolia',
+//     currency: 'ETH',
+//     explorerUrl: "https://sepolia.arbiscan.io/",
+//     rpcUrl : "https://endpoints.omniatech.io/v1/arbitrum/sepolia/public	"
+// }
 
-const arbitrum_sepolia = {
-    chainId: 421614,
-    name: 'Arbitrum Sepolia',
-    currency: 'ETH',
-    explorerUrl: "https://sepolia.arbiscan.io/",
-    rpcUrl : "https://endpoints.omniatech.io/v1/arbitrum/sepolia/public	"
+const arbitrum = {
+    chainId: 42161,
+    name: "Arbitrum",
+    currency: "ETH",
+    explorerUrl: "https://arbiscan.io",
+    rpcUrl: "https://arb1.arbitrum.io/rpc",
 }
 
-const arbutrum = {
-    chainId: 42161,
-    name: 'Arbitrum',
-    currency: 'ETH',
-    explorerUrl: 'https://arbiscan.io',
-    rpcUrl: 'https://arb1.arbitrum.io/rpc'
-    }
+const flowEvm = {
+    chainId: 747,
+    name: "EVM on Flow",
+    currency: "FLOW",
+    explorerUrl: "https://evm.flowscan.io/",
+    rpcUrl: "https://mainnet.evm.nodes.onflow.org",
+}
 
 const metadata = {
-  name: 'My Website',
-  description: 'My Website description',
-  url: 'https://mywebsite.com', // origin must match your domain & subdomain
-  icons: ['https://avatars.mywebsite.com/']
+    name: "My Website",
+    description: "My Website description",
+    url: "https://mywebsite.com", // origin must match your domain & subdomain
+    icons: ["https://avatars.mywebsite.com/"],
 }
 
-
 const ethersConfig = defaultConfig({
-  /*Required*/
-  metadata,
+    /*Required*/
+    metadata,
 
-  /*Optional*/
-  // enableEIP6963: true, // true by default
-  enableInjected: true, // true by default
-  enableCoinbase: true, // true by default
-  rpcUrl: '...', // used for the Coinbase SDK
-  defaultChainId: 1, // used for the Coinbase SDK
+    /*Optional*/
+    // enableEIP6963: true, // true by default
+    enableInjected: true, // true by default
+    enableCoinbase: true, // true by default
+    rpcUrl: "...", // used for the Coinbase SDK
+    defaultChainId: 1, // used for the Coinbase SDK
 })
 
 createWeb3Modal({
-  ethersConfig,
-  chains: [arbitrum_sepolia],
-  projectId,
-  enableAnalytics: true // Optional - defaults to your Cloud configuration
+    ethersConfig,
+    chains: [arbitrum, flowEvm],
+    projectId,
+    enableAnalytics: true, // Optional - defaults to your Cloud configuration
 })
 
-
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <AppRoutes />
+            </BrowserRouter>
+        </>
+    )
 }
 
-export default App;
+export default App
